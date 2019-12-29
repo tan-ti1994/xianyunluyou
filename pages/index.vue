514e6663583407b27ed7575c8474eea046e96ac9
tan-ti1994 <TCT2016Y@163.com>
2019-12-29 13:44:05 +0800

    “第一次提交”

---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+  <el-carousel height="400px" :interval="5000" arrow="always">
+    <el-carousel-item v-for="(item, index) in banners" :key="index">
+      <img :src="`${$axios.defaults.baseURL + item.url}`" alt="" />
+    </el-carousel-item>
+  </el-carousel>
+  <div @click="dianji">按钮</div>
+  </div>
+</template>
+
+<script>
+import { lightpink } from "color-name";
+export default {
+  data() {
+    return {
+      banners: []
+    };
+  },
+  mounted() {
+    this.$axios({
+      url: "/scenics/banners"
+    }).then(res => {
+      console.log(res);
+
+      // const{data}=res.data;
+      this.banners = res.data.data;
+    });
+  },
+  methods: {
+    dianji(){
+      this.$store.commit('user/statearr','谭总裁')
+      // this.$store.commit('user/setname','shadiao')
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+img {
+  width: 100%;
+  height: 100%;
+ 
+}
+
+</style>
---
+++ store/user.js
@@ -0,0 +1,11 @@
+export const state={
+    name:'谭总'
+};
+export const mutations={
+  statearr(state,data){
+state.name=data
+  }
+};
+export const actions={
+
+}
